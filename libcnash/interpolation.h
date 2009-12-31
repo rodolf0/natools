0ec99dba59698c73796aa69ad1861355c7d21a65
Rodolfo Granata <warlock.cc@gmail.com>
2009-12-30 23:45:11 -0300

    - add interpolation header.

---
+++ libcnash/interpolation.h
@@ -0,0 +1,13 @@
+#ifndef _INTERPOLATION_H_
+#define _INTERPOLATION_H_
+
+#include "cnash.h"
+#include "function.h"
+
+int interpolate_newton(vector2_t *x, int n, double x0, double *r);
+int finite_difference(function_t *f, int n, double x0, double h, double *r);
+int interpolate_lagrange(vector2_t *x, int n, double x0, double *r);
+
+#endif /* _INTERPOLATION_H_ */
+
+/* vim: set sw=2 sts=2 : */
