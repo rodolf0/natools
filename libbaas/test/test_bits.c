b9e9c3a6085e74c9b478b2b782bbfbf416837995
Rodolfo Granata <warlock.cc@gmail.com>
2014-04-18 00:32:58 -0400

    - refactor bits tests

--- libbaas/test/test_bits.c
+++ libbaas/test/test_bits.c
@@ -10,37 +10,25 @@
 #include "baas/bits.h"
 
 void test_ints_to_bits(void) {
-  char bits[33];
-  int len;
+  struct {
+    int32_t num;
+    char    bits[33];
+  } testv[] = {
+    {0, "0"},
+    {1, "1"},
+    {2, "10"},
+    {123749123, "111011000000100001100000011"},
+    {789453645, "101111000011100001101101001101"},
+    {475895234, "11100010111011001010111000010"},
+    {864502334, "110011100001110100001000111110"},
+    {324506094, "10011010101111001000111101110"},
+  };
 
-  int32_t num1 = 123749123;
-  int32_t num2 = 789453645;
-  int32_t num3 = 475895234;
-  int32_t num4 = 864502334;
-  int32_t num5 = 324506094;
-
-  len = int_to_bits(0, bits, sizeof(bits));
-  assert(len == 1);
-  assert(!strcmp(bits + 32 - len, "0"));
-
-  len = int_to_bits(1, bits, sizeof(bits));
-  assert(len == 1);
-  assert(!strcmp(bits + 32 - len, "1"));
-
-  len = int_to_bits(2, bits, sizeof(bits));
-  assert(len == 2);
-  assert(!strcmp(bits + 32 - len, "10"));
-
-  len = int_to_bits(num1, bits, sizeof(bits));
-  assert(!strcmp(bits + 32 - len, "111011000000100001100000011"));
-  len = int_to_bits(num2, bits, sizeof(bits));
-  assert(!strcmp(bits + 32 - len, "101111000011100001101101001101"));
-  len = int_to_bits(num3, bits, sizeof(bits));
-  assert(!strcmp(bits + 32 - len, "11100010111011001010111000010"));
-  len = int_to_bits(num4, bits, sizeof(bits));
-  assert(!strcmp(bits + 32 - len, "110011100001110100001000111110"));
-  len = int_to_bits(num5, bits, sizeof(bits));
-  assert(!strcmp(bits + 32 - len, "10011010101111001000111101110"));
+  for (size_t i = 0; i < sizeof(testv) / sizeof(testv[0]); ++i) {
+    char bits[33];
+    int len = int_to_bits(testv[i].num, bits, sizeof(bits));
+    assert(!strcmp(bits + 32 - len, testv[i].bits));
+  }
 }
 
 void test_bitcount(void) {
@@ -51,9 +39,9 @@ void test_bitcount(void) {
 #define ITERATIONS 100
 int main(void) {
   int i;
+  test_ints_to_bits();
   for (i = 1; i <= ITERATIONS; i++) {
     fprintf(stderr, "\rtesting ... %d%%", 100*i/ITERATIONS);
-    test_ints_to_bits();
     test_bitcount();
   }
   fprintf(stderr, "\n");
