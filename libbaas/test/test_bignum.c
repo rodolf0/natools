9b3e24fb825030e25ee08928164d8ff8e9150f4a
Rodolfo Granata <warlock.cc@gmail.com>
2014-04-18 00:20:16 -0400

    - adjust bignum tests

--- libbaas/test/test_bignum.c
+++ libbaas/test/test_bignum.c
@@ -10,56 +10,61 @@
 
 static const char digit[] = "0123456789";
 
-void test_bignum(void) {
-  char num1[8], num2[8], *result;
-  int i, nlen;
-
-  nlen = 1 + random() % 7;
-  for (i = 0; i < nlen; i++)
-    num1[i] = digit[random() % 9];
-  num1[nlen-1] = '\0';
-
-  nlen = 1 + random() % 7;
-  for (i = 0; i < nlen; i++)
-    num2[i] = digit[random() % 9];
-  num2[nlen-1] = '\0';
-
-  result = big_add(num1, num2);
-  assert(atoi(result) == atoi(num1) + atoi(num2));
-  free(result);
-
-  result = big_mul(num1, num2);
-  assert(atoi(result) == atoi(num1) * atoi(num2));
-  free(result);
+void random_bigint(char *n, size_t d) {
+  size_t len = 2 + random() % (d-2);
+  for (size_t i = 0; i < len; ++i)
+    n[i] = digit[random() % 9];
+  n[len-1] = '\0';
 }
 
-void test_bignum2(void) {
-  char *result;
+void test_big_add(void) {
+  char n1[18], n2[18];
+  random_bigint(n1, 18);
+  random_bigint(n2, 18);
+  char *r = big_add(n1, n2);
+  assert(strtoull(r, NULL, 10) ==
+         strtoull(n1, NULL, 10) + strtoull(n2, NULL, 10));
+  free(r);
+}
+
+void test_big_mul(void) {
+  char n1[10], n2[8];
+  random_bigint(n1, 10);
+  random_bigint(n2, 8);
+  char *r = big_mul(n1, n2);
+  assert(strtoull(r, NULL, 10) ==
+         strtoull(n1, NULL, 10) * strtoull(n2, NULL, 10));
+  free(r);
+}
+
+void test_bignum(void) {
   const char *num1 = "98143278943523452345789012347890543257890123473890543257"
                      "8902341789054325789012347890234578902345";
   const char *num2 = "98723450823475871234089754320987234897052435789013240987"
                      "543209871234780234508971234";
+  char *result;
 
   result = big_add(num1, num2);
-  assert(0==strcmp(result, "09814327894353332469087135993501395223332222219738"
-                           "02485014691355030041868998883582670469087873579"));
+  assert(!strcmp(result, "09814327894353332469087135993501395223332222219738"
+                         "02485014691355030041868998883582670469087873579"));
   free(result);
 
   result = big_mul(num1, num2);
-  assert(0 == strcmp(result, "968904317243561250527515505659747936287024828385"
-                             "727972083025367497594773491424682907460548592048"
-                             "978383210171914895096073300883425357575387395841"
-                             "73736227468144009012422126900143730"));
+  assert(!strcmp(result, "968904317243561250527515505659747936287024828385"
+                         "727972083025367497594773491424682907460548592048"
+                         "978383210171914895096073300883425357575387395841"
+                         "73736227468144009012422126900143730"));
   free(result);
 }
 
 #define ITERATIONS 10000
 int main(void) {
   int i;
+  test_bignum();
   for (i = 1; i <= ITERATIONS; i++) {
     fprintf(stderr, "\rtesting ... %d%%", 100*i/ITERATIONS);
-    test_bignum();
-    test_bignum2();
+    test_big_add();
+    test_big_mul();
   }
   fprintf(stderr, "\n");
 
